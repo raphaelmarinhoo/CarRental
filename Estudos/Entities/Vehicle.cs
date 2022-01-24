5de68f9b911052752142eaa7a195cd62fd59c272
Raphael Lucas <raphael.lukas.m@gmail.com>
2022-01-24 15:31:50 -0300

    Adicionar arquivos de projeto.

---
+++ Estudos/Entities/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Estudos.Entities
+{
+    internal class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
